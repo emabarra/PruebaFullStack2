220d37adfc0d327fe8569deacd15a134b6eb2f69
emabarra <em.barra@duocuc.cl>
2026-09-05 16:24:52 -0400

    Se realiza login

--- login/login.js
+++ login/login.js
@@ -1,13 +1,12 @@
-
 function loguear() {
     let user = document.getElementById("usuario").value;
     let pass = document.getElementById("contraseña").value;
 
-    if (user == "user" && pass == "1234") {
-        window.location = "../perfil-usuario/usuario.html";
+    if (user == "usuario" && pass == "1234") {
+        window.location = "../perfil-usuario/usuario.html"
     } else if (user == "admin" && pass == "1234") {
-        window.location = "../perfil-admin/admin.html";
+        window.location = "../perfil-admin/admin.html"
     } else {
-        alert("Datos incorrectos");
+        alert("Contraseña o Usuario Incorrecto");
     }
 }
