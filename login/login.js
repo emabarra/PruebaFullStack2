61799f97969da672c8b604a8949f9c58fdc9ea6f
emabarra <em.barra@duocuc.cl>
2026-09-05 14:47:33 -0400

    Se añade funcion de loguear a login.js ademas de la carpeta perfil-admin con sus respectivos html, css y js

--- login/login.js
+++ login/login.js
@@ -1,4 +1,13 @@
 
 function loguear() {
+    let user = document.getElementById("usuario").value;
+    let pass = document.getElementById("contraseña").value;
 
+    if (user == "user" && pass == "1234") {
+        window.location = "../perfil-usuario/usuario.html";
+    } else if (user == "admin" && pass == "1234") {
+        window.location = "../perfil-admin/admin.html";
+    } else {
+        alert("Datos incorrectos");
+    }
 }
